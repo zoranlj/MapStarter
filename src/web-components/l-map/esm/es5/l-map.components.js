8d5347d89a3c4d1dfc467b7b65a3dbbf4bccbc1d
Zoran Ljubinkovic <zoranlj@gmail.com>
2018-09-27 15:27:16 +0200

    new l-map component added

--- src/web-components/l-map/esm/es5/l-map.components.js
+++ src/web-components/l-map/esm/es5/l-map.components.js
@@ -1,3 +1,3 @@
 // LMap: Host Data, ES Module/ES5 Target
 
-export var LMap = ["l-map",function(o){return(o.scoped?import("./jiwpp8ni.sc.js"):import("./jiwpp8ni.js")).then(function(m){return m.LMap})},1,[["LMapHTMLElement",7],["center",1,0,1,2],["iconUrl",1,0,"icon-url",2],["locations",1,0,1,2],["maxZoom",1,0,"max-zoom",2],["minZoom",1,0,"min-zoom",2],["tileLayerUrl",1,0,"tile-layer-url",2],["zoom",1,0,1,2]],1];
+export var LMap = ["l-map",function(o){return(o.scoped?import("./q9qfnby7.sc.js"):import("./q9qfnby7.js")).then(function(m){return m.LMap})},1,[["LMapHTMLElement",7],["center",1,0,1,2],["iconUrl",1,0,"icon-url",2],["locations",1,0,1,2],["maxZoom",1,0,"max-zoom",2],["minZoom",1,0,"min-zoom",2],["tileLayerUrl",1,0,"tile-layer-url",2],["zoom",1,0,1,2]],1];
