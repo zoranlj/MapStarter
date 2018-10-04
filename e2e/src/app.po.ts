999a9ad4eec9ff3087faddc4a79b391c2b7172d4
Zoran Ljubinkovic <zoranlj@gmail.com>
2018-10-04 11:40:18 +0200

    e2e tests

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -5,7 +5,7 @@ export class AppPage {
     return browser.get('/');
   }
 
-  getParagraphText() {
-    return element(by.deepCss('app-root ion-content')).getText();
+  getTitleText() {
+    return element(by.tagName('ion-title')).getText();
   }
 }
